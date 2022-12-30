d26f4685b107b164ec2dbb48ef687ba9154cb5de
Ezekiel <umanmah234@gmail.com>
2022-12-30 03:26:51 -0500

    create music clips data

---
+++ client/src/Components/Music Pads/music.js
@@ -0,0 +1,26 @@
+import A from '../../assets/piano/A.mp3';
+import B from '../../assets/piano/B.mp3';
+import Bb from '../../assets/piano/Bb.mp3';
+import C from '../../assets/piano/C.mp3';
+import D from '../../assets/piano/D.mp3';
+import Db from '../../assets/piano/Db.mp3';
+import E from '../../assets/piano/E.mp3';
+import Eb from '../../assets/piano/Eb.mp3';
+import F from '../../assets/piano/F.mp3';
+import G from '../../assets/piano/G.mp3';
+import Gb from '../../assets/piano/Gb.mp3';
+
+// prettier-ignore
+export const pianoClips = [
+  {note: 'A', audioID: 'Q', src: A},
+  {note: 'B', audioID: 'W', src: B},
+  {note: 'Bb', audioID: 'E', src: Bb},
+  {note: 'C', audioID: 'R', src: C},
+  {note: 'D', audioID: 'A', src: D},
+  {note: 'Db', audioID: 'S', src: Db},
+  {note: 'E', audioID: 'D', src: E},
+  {note: 'Eb', audioID: 'F', src: Eb},
+  {note: 'F', audioID: 'Z', src: F},
+  {note: 'G', audioID: 'X', src: G},
+  {note: 'Gb', audioID: 'C', src: Gb},
+];
