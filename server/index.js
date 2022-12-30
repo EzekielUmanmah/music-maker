8c23708f5fe4488ef6a80d3a6d2ad7a2f06a0677
Ezekiel <umanmah234@gmail.com>
2022-12-30 03:28:06 -0500

    add seed data route

--- server/index.js
+++ server/index.js
@@ -3,11 +3,15 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
 
+import { seed } from './controller.js';
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => res.send("You've reached the music maker API!"));
+
+app.post('/seed', seed);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
