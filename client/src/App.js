bdcbb4ca5d4b4ff64bb9ec1ddeffa1d6bbf7193d
Ezekiel <umanmah234@gmail.com>
2023-01-01 05:52:08 -0500

    add initial App routes/components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,34 @@
-// import './App.css';
+import React, { useRef, useState, createContext } from 'react';
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
 
-function App() {
+import LandingPage from './Components/LandingPage/LandingPage';
+import Dashboard from './Components/Dashboard/Dashboard';
+import { GlobalStyle } from './styles';
+
+export const AppContext = createContext();
+
+export function App() {
+  const [state, setState] = useState({
+    user: null,
+    str: 'Online',
+    status: true,
+    mute: false,
+    refsArray: useRef([]),
+    music: [],
+    isRecord: false,
+  });
+  // console.log('app: ', state);
   return (
-    <div className='App'>
-      <h1>Hello</h1>
+    <div className='app'>
+      <AppContext.Provider value={[state, setState]}>
+        <GlobalStyle />
+        <Router>
+          <Routes>
+            <Route path='/' element={<LandingPage />} />
+            <Route path='/player' element={<Dashboard />} />
+          </Routes>
+        </Router>
+      </AppContext.Provider>
     </div>
   );
 }
-
-export default App;
