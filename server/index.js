f4362bc4694bc4eaf1a0fdd7a25da23b9239fa0f
Ezekiel <umanmah234@gmail.com>
2023-01-04 19:03:26 -0500

    add routing for clips

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ dotenv.config();
 
 import { seed } from './controllers/seed.js';
 import userRoutes from './routes/user.js';
+import clipRoutes from './routes/clips.js';
 
 const app = express();
 app.use(cors());
@@ -14,6 +15,7 @@ app.get('/', (req, res) => res.send("You've reached the music maker API!"));
 
 app.post('/seed', seed);
 app.use('/user', userRoutes);
+app.use('/clips', clipRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
